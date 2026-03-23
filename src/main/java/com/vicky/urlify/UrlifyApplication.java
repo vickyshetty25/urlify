8f593b6b1468897db07154f8a1537cc93b7e926a
Vicky Shetty <vickyshetty@Vickys-Mac-mini.local>
2026-03-23 16:50:44 +0530

    initial: Spring Boot project setup

---
+++ src/main/java/com/vicky/urlify/UrlifyApplication.java
@@ -0,0 +1,13 @@
+package com.vicky.urlify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicky/urlify/UrlifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.urlify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
